752ee5372e5c4be1a711cef27449cc98f4573226
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-05-27 01:45:04 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from require('lodash.throttle');
 
+const STORAGE_KEY = 'feedback-form-state';
+
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(event) {
+    event.preventDefault();
+
+    if (!event.target.email.vaule || !event.target.message.vaule) {
+        alert('Заповніть усі поля форми');
+        return;
+    }
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+
+}
+
+outputFromLocalStorage();
+
+function outputFromLocalStorage() {
+    const outputData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (outputData) {
+        refs.email.vaule = outputData.email;
+        refs.message.vaule = outputData.message;
+    }
+}
