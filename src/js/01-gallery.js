193242d393c8baa22711679a482f338880b10ab7
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-05-27 00:30:50 +0300

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,70 @@
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector(".gallery");
+
+const imagesMarkup = createImagesMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+
+galleryContainer.addEventListener("click", onImagesContainerClick);
+
+function createImagesMarkup(galleryItems) {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+  return markup;
+}
+
+function onImagesContainerClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const selectedImage = event.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `<img src = "${selectedImage}" width = "600" height = "400">`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscKeyPress);
+      },
+
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscKeyPress);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onEscKeyPress(e) {
+    if (e.code !== "Escape") return;
+
+    instance.close();
+  }
+}
+
+const lightbox = new SimpleLightbox('.gallery a' , {
+    captionDelay: 250,
+  captionsData: 'alt',
+});
