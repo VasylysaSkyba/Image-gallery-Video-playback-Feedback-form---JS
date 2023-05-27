7161f5270e4bd0697232ee21ab54da5d74c7a3f5
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-05-27 18:27:05 +0300

    доопрацювання 3ї роботи

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,34 +2,41 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 
-
 const refs = {
-    form: document.querySelector('.feedback-form'),
-    email: document.querySelector('.feedback-form input'),
-    message: document.querySelector('.feedback-form textarea'),
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
 };
 
 refs.form.addEventListener('input', throttle(onFormInput, 500));
 
-function onFormInput(event) {
-    event.preventDefault();
-
-    if (!event.target.email.vaule || !event.target.message.vaule) {
-        alert('Заповніть усі поля форми');
-        return;
-    }
-    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
-    event.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
-
+function onFormInput() {
+  const formData = JSON.stringify({
+    email: refs.email.value.trim(),
+    message: refs.message.value.trim(),
+  });
+  localStorage.setItem(STORAGE_KEY, formData);
 }
 
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (!event.target.email.value || !event.target.message.value) {
+    alert('Заповніть усі поля форми');
+    return;
+  }
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
 outputFromLocalStorage();
 
 function outputFromLocalStorage() {
-    const outputData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    if (outputData) {
-        refs.email.vaule = outputData.email;
-        refs.message.vaule = outputData.message;
-    }
+  const outputData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (outputData) {
+    refs.email.value = outputData.email;
+    refs.message.value = outputData.message;
+  }
 }
