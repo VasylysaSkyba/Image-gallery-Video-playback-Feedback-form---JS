4e50dee2da31f9e91efe96a1884acae160d08f4c
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-05-27 17:40:35 +0300

    виправлення 1ї задачі

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,63 +7,30 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
-const galleryContainer = document.querySelector('.gallery');
+const galleryContainer = document.querySelector(".gallery");
 
 const imagesMarkup = createImagesMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
-
-galleryContainer.addEventListener('click', onImagesContainerClick);
+galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
 
 function createImagesMarkup(galleryItems) {
   const markup = galleryItems
     .map(({ preview, original, description }) => {
       return `
-        <li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
+      <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" 
+      src="${preview}" 
+      data-sourse = "${original}"
       alt="${description}"
-    />
-  </a>
+       />
+   </a>
 </li>`;
     })
-    .join('');
+    .join("");
   return markup;
 }
 
-function onImagesContainerClick(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-
-  const selectedImage = event.target.dataset.source;
-
-  const instance = basicLightbox.create(
-    `<img src = "${selectedImage}" width = "600" height = "400">`,
-    {
-      onShow: instance => {
-        window.addEventListener('keydown', onEscKeyPress);
-      },
-
-      onClose: instance => {
-        window.removeEventListener('keydown', onEscKeyPress);
-      },
-    }
-  );
-
-  instance.show();
-
-  function onEscKeyPress(e) {
-    if (e.code !== 'Escape') return;
-
-    instance.close();
-  }
-}
-
-const lightbox = new SimpleLightbox('.gallery a', {
+const lightbox = new SimpleLightbox(".gallery a", {
   captionDelay: 250,
-  captionsData: 'alt',
+  captionsData: "alt",
 });
